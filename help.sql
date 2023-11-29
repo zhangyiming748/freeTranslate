66a47d7735e1542e7c885c39ef8454017a1f9aca
zen <zhangyiming748@gmail.com>
2023-11-29 09:24:50 +0800

    等待时间还需要调整

---
+++ help.sql
@@ -0,0 +1,2 @@
+select src,dst,create_time from history order by id desc;
+select dst_name from save order by size desc;
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"freeTranslate/baidu"
 	"freeTranslate/model"
 	"freeTranslate/storage/mysql"
 	"freeTranslate/util"
@@ -17,7 +18,7 @@ func init() {
 func main() {
 	cache := make(map[string]string)
 	before := util.ReadByLine("before.srt")
-	after, _ := os.OpenFile("after.srt", 2|8|512, 0777)
+	after, _ := os.OpenFile("after.srt", os.O_RDWR|os.O_APPEND|os.O_CREATE, 0777)
 	for i := 0; i < len(before); i += 4 {
 		after.WriteString(fmt.Sprintf("序号 : %s\n", before[i]))
 		after.WriteString(fmt.Sprintf("时间轴 : %s\n", before[i+1]))
@@ -25,14 +26,22 @@ func main() {
 		if dst, ok := cache[src]; ok {
 			after.WriteString(fmt.Sprintf("字幕 : %s\n", dst))
 		} else {
-			//dst = baidu.AskBaidu(src)
-			dst = "c"
+			dst = baidu.AskBaidu(src)
+			//dst = "c"
 			cache[src] = dst
 			after.WriteString(fmt.Sprintf("字幕 : %s\n", dst))
-			time.Sleep(1500 * time.Microsecond)
+			h := new(model.History)
+			h.Src = src
+			h.Dst = dst
+			_, err := h.InsertOne()
+			if err != nil {
+				fmt.Println("数据库插入错误")
+				continue
+			}
 		}
 		after.WriteString(fmt.Sprintf("空行 : %s\n", before[i+3]))
 		after.Sync()
+		time.Sleep(10 * time.Second)
 		fmt.Printf("循环一次后cache的情况: %+v\n", cache)
 	}
 }
