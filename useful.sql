9d578e51f0eed58e4615deb2ac71e18799a4523c
mi wsl <zhangyiming748@gmail.com>
2023-12-09 17:38:39 +0800

    fix

--- useful.sql
+++ useful.sql
@@ -1,3 +1,3 @@
 select src,dst from history order by create_time desc ;
-DELETE FROM history WHERE history.dst = "";
 DELETE FROM history WHERE dst LIKE '%%';
+DELETE FROM history WHERE dst LIKE '';
