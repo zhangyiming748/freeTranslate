543b15e9712116d97f3b230abe680956edc03421
zen <zhangyiming748@gmail.com>
2024-02-29 09:10:25 +0800

    添加程序出错条件

--- translateShell/bing.go
+++ translateShell/bing.go
@@ -12,7 +12,7 @@ func TransByBing(proxy, language, src string, ans chan string) {
 	cmd := exec.Command("trans", "-brief", "-proxy", proxy, language, src)
 	slog.Debug("Bing", slog.String("命令原文", fmt.Sprintf("%s", cmd)))
 	output, err := cmd.CombinedOutput()
-	if err != nil || strings.Contains(string(output), "u001b") || strings.Contains(string(output), "Didyoumean") {
+	if err != nil || strings.Contains(string(output), "u001b") || strings.Contains(string(output), "Didyoumean") || strings.Contains(string(output), "Connectiontimedout") {
 		slog.Error("命令执行出错", slog.String("错误原文", err.Error()))
 		time.Sleep(3 * time.Second)
 		TransByBing(proxy, language, src, ans)
--- translateShell/google.go
+++ translateShell/google.go
@@ -12,7 +12,7 @@ func TransByGoogle(proxy, language, src string, ans chan string) {
 	cmd := exec.Command("trans", "-brief", "-proxy", proxy, language, src)
 	slog.Debug("Trans", slog.String("命令原文", fmt.Sprintf("%s", cmd)))
 	output, err := cmd.CombinedOutput()
-	if err != nil || strings.Contains(string(output), "u001b") || strings.Contains(string(output), "Didyoumean") {
+	if err != nil || strings.Contains(string(output), "u001b") || strings.Contains(string(output), "Didyoumean") || strings.Contains(string(output), "Connectiontimedout") {
 		slog.Error("命令执行出错", slog.String("错误原文", err.Error()))
 		time.Sleep(3 * time.Second)
 		TransByGoogle(proxy, language, src, ans)
